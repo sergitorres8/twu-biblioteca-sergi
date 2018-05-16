4645fbaf7a337c57a3ec8e1d8574f56e89c81b11
sergitorres8 <sergitfsancarlos@hotmail.com>
2018-05-16 11:23:14 +0200

    refactoring and testing nav

--- test/com/twu/biblioteca/NavigatorTest.java
+++ test/com/twu/biblioteca/NavigatorTest.java
@@ -27,9 +27,63 @@ public class NavigatorTest {
 
     @Test
     public void shouldCallCheckOutWhenInputIs2() {
+        givenThatWeWantToCheckOutAExistingBook();
+        nav.menu(2);
+        Mockito.verify(lib, Mockito.atLeastOnce()).checkout(libraryItem);
+    }
+
+    @Test
+    public void shouldNotCallCheckOutWhenBookDoesNotExist() {
+        lib =  Mockito.mock(Library.class);
+        libraryItem = new Book("Potter", "hj", 97);
+        inputParser = Mockito.mock(InputParser.class);
+
+        Mockito.when(inputParser.askForForTitle()).thenReturn("Potter");
+        Mockito.when(lib.getBookByTitle("Potter")).thenReturn(Optional.empty());
+        Mockito.when(lib.checkout(libraryItem)).thenReturn(Optional.of(libraryItem));
+        Mockito.when(lib.getBooks()).thenReturn(Arrays.asList((Book) libraryItem));
+
+        nav = new Navigator(lib, inputParser);
+        nav.user = new User("000-0001","0001","Sergi Torres", "storres@thoughtworks.com", "959503509");
+        nav.menu(2);
+        Mockito.verify(lib, Mockito.never()).checkout(libraryItem);
+    }
+
+    @Test
+    public void shouldNotReturnBookWhenBookExistsButWasNotCheckedOutPreviously(){
         lib =  Mockito.mock(Library.class);
         libraryItem = new Book("Harry Potter", "J. K. Rowling", 1997);
-        book = Mockito.mock(Book.class);
+        inputParser = Mockito.mock(InputParser.class);
+
+        Mockito.when(lib.getBooks()).thenReturn(Arrays.asList((Book) libraryItem));
+        Mockito.when(inputParser.askForForTitle()).thenReturn("Harry Potter");
+        Mockito.when(lib.getBookByTitle("Harry Potter")).thenReturn(Optional.of((Book) libraryItem));
+        Mockito.when(lib.returnBook(libraryItem)).thenReturn(Optional.of(libraryItem));
+
+        nav = new Navigator(lib, inputParser);
+        nav.user = new User("000-0001","0001","Sergi Torres", "storres@thoughtworks.com", "959503509");
+        nav.menu(3);
+        Mockito.verify(lib, Mockito.never()).returnBook(libraryItem);
+    }
+
+    @Test
+    public void shouldReturnBookWhenBookExistsAndWasCheckedOutPreviously(){
+        nav = givenThatWeCorrectlyCheckedOutABook();
+        Mockito.when(lib.returnBook(libraryItem)).thenReturn(Optional.of(libraryItem));
+        nav.menu(3);
+        Mockito.verify(lib, Mockito.atLeastOnce()).returnBook(libraryItem);
+    }
+
+    private Navigator givenThatWeCorrectlyCheckedOutABook() {
+        givenThatWeWantToCheckOutAExistingBook();
+        nav.menu(2);
+        Mockito.verify(lib, Mockito.atLeastOnce()).checkout(libraryItem);
+        return nav;
+    }
+
+    private void givenThatWeWantToCheckOutAExistingBook() {
+        lib =  Mockito.mock(Library.class);
+        libraryItem = new Book("Harry Potter", "J. K. Rowling", 1997);
         inputParser = Mockito.mock(InputParser.class);
 
         Mockito.when(inputParser.askForForTitle()).thenReturn("Harry Potter");
@@ -39,21 +93,8 @@ public class NavigatorTest {
 
         nav = new Navigator(lib, inputParser);
         nav.user = new User("000-0001","0001","Sergi Torres", "storres@thoughtworks.com", "959503509");
-        nav.menu(2);
-        Mockito.verify(lib, Mockito.atLeastOnce()).checkout(libraryItem);
     }
 
-    @Test
-    public void shouldCallReturnBookWhenInputIs2(){
-        lib =  Mockito.mock(Library.class);
-        libraryItem = new Book("Harry Potter", "J. K. Rowling", 1997);
-        book = Mockito.mock(Book.class);
-        inputParser = Mockito.mock(InputParser.class);
-
-        Mockito.when(lib.getBooks()).thenReturn(Arrays.asList((Book) libraryItem));
-
-
-    }
 
     private void givenThereIsOneBookInTheLibrary() {
         lib = Mockito.mock(Library.class);
